751868eaf843b98a80575e639dbf29021ff6fe64
Mario Visic <mario@mariovisic.com>
2013-03-03 11:58:55 +1100

    Add some integration testing

---
+++ spec/support/test_sites.rb
@@ -0,0 +1,10 @@
+# Add a list of sites to test here:
+
+def sites
+  [
+    #'https://www.google.com.au/',
+    #'https://www.desktoppr.co/',
+    #'http://www.giftoppr.co/',
+    #'http://au.yahoo.com/'
+  ]
+end
---
+++ spec/wbench/integration_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+require File.expand_path(File.dirname(__FILE__)) + '/../support/test_sites'
+sites = @sites
+
+describe 'wbench' do
+  sites.each do |site|
+    it "should return information from #{site} in chrome" do
+      expect { WBench::Benchmark.run(site, :loops => 1) }.to_not raise_exception
+    end
+
+    it "should return information from #{site} in firefox" do
+      expect { WBench::Benchmark.run(site, :browser => :firefox, :loops => 1) }.to_not raise_exception
+    end
+  end
+end
