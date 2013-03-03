c2165f5672d9bb3565df282734d14c3965d85e7f
Mario Visic <mario@mariovisic.com>
2013-03-03 16:50:11 +1100

    Add testing for user agent string

--- spec/wbench/integration_spec.rb
+++ spec/wbench/integration_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 require File.expand_path(File.dirname(__FILE__)) + '/../support/test_sites'
 
 describe 'wbench' do
-  sites.each do |site|
+  sites.each_with_index do |site, index|
+    if index == 0
+      it 'accepts a user agent string' do
+        expect { WBench::Benchmark.run(sites.first, :loops => 1, :user_agent => 'a mobile browser') }.to_not raise_exception
+      end
+    end
+
     it "should return information from #{site} in chrome" do
       expect { WBench::Benchmark.run(site, :loops => 1) }.to_not raise_exception
     end
